ef967c1953020abdfe9a9f286450be90d2d6c0be
Tem <54119870+Teampris@users.noreply.github.com>
2023-03-22 01:44:25 +0100

    Add play round function

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let playerPoint = 0;
+let computerPoint = 0;
+
 function getComputerChoice() {
     let random = Math.floor(Math.random() * 3) + 1;
     let choice;
@@ -20,5 +23,29 @@ function getPlayerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-
+    if (playerSelection == 'rock' && computerSelection == 'paper') {
+        computerPoint++;
+        return 'You lose! Paper beats rock';        
+    } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+        playerPoint++;
+        return 'You win! Rock beats scissors';        
+    } else if (playerSelection == 'paper' && computerSelection == 'rock') {
+        playerPoint++;
+        return 'You win! Paper beats rock';        
+    } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
+        computerPoint++;
+        return 'You lose! scissors beats paper';        
+    } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+        playerPoint++;
+        return 'You win! scissors beats paper';        
+    } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
+        playerPoint++;
+        return 'You lose! scissors beats rock';        
+    } else {
+        return 'Tie!';
+    }
 }
+
+const playerSelection = getPlayerChoice();
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
